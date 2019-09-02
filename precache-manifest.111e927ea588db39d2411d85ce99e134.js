65cb992b7f97533854ebc5bd6c38671b33d66668
Anass Bouchtaoui <anass.b@globres.com>
2019-09-02 15:23:02 +0100

    Updates

---
+++ precache-manifest.111e927ea588db39d2411d85ce99e134.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca6946da98f801cc9d04dd8592508130",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "df305819f07416a12ce0",
+    "url": "/monsters-rolodex/static/css/main.d3975f55.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "df305819f07416a12ce0",
+    "url": "/monsters-rolodex/static/js/main.09032db4.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
